b67888825ea71309b877bca818baaa10935078ae
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-10 15:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-//your code here
 
+const input = document.querySelector("#newTodoInput");
+const addBtn = document.querySelector("#addTodoBtn")
+const orderList  = document.querySelector("#todoList");
+
+addBtn.addEventListener("click",(e)=>{
+  
+
+  if(input.value){
+    const listItem = document.createElement("li");
+
+    listItem.innerText = input.value;
+  
+    orderList.appendChild(listItem);
+  }
+  
+
+
+  
+})
